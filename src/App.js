bffc43794a11430570a5a99090d8bdf7dada43f6
soujanya ghosh <soujanyamax@gmail.com>
2022-04-21 17:18:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Movie from './components/Movie';
+
+const FEATURED_API = "http://api.themoviedb.org/3/trending/all/week?api_key=9fcf7a3dcf6947a420682da4c7f85220&language=en-US";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [movies, setMovies] = useState([]);
+
+  useEffect( () => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      })
+  }, [])
+
+  return <div className='movie-container'>
+    {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />
+    )}
     </div>
-  );
 }
 
 export default App;
